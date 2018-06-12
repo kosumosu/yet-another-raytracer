60741cc8baddbfbf0e0469ccd0bb427c41300cfe
Roman Zavalov <japangod@yandex.ru>
2018-06-12 23:07:54 +0300

    Implemented more correct rgb-color-to-importance  function (BT. 709).

--- yet-another-raytracer/color_functions.hpp
+++ yet-another-raytracer/color_functions.hpp
@@ -7,6 +7,7 @@ namespace color
 	color_real inline get_importance(const color_rgbx & color)
 	{
 		// simple averaging for now
-		return (color[0] + color[1] + color[2]) / color_real(3.0);
+		//return (color[0] + color[1] + color[2]) / color_real(3.0);
+		return color[0] * color_real(0.2126) + color[1] * color_real(0.7152) + color[2] * color_real(0.0722);
 	}
 }
