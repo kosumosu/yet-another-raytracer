37ea884508907054b210439c982b29281301d5e5
Roman Zavalov <japangod@yandex.ru>
2021-05-30 00:03:25 +0300

    Removed incorrect unused function.

--- yet-another-raytracer/helper.hpp
+++ yet-another-raytracer/helper.hpp
@@ -90,14 +90,6 @@ namespace math
 		return std::min(max_value, std::max(min_value, value));
 	}
 
-	template <typename TInt, typename TFloat>
-	TInt fast_floor(const TFloat & float_value)
-	{
-		static_assert(std::is_floating_point<TFloat>::value, "Argument must be floating point!");
-		static_assert(std::is_integral<TInt>::value && !std::is_same<TInt, bool>::value, "Return value must be of integral type but not bool.");
-		return float_value >= TFloat(0) ? static_cast<TInt>(float_value) : static_cast<TInt>(float_value) - TInt(1);
-	}
-
 	template<typename T>
 	bool is_same_sign(const T & a, const T & b)
 	{
